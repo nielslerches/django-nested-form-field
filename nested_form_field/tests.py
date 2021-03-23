a720599908aebdb7b19afd3872a0291a80ee00d8
Niels Lerche Sørensen <nielslerches@gmail.com>
2021-03-23 22:43:49 +0100

    add test

--- nested_form_field/tests.py
+++ nested_form_field/tests.py
@@ -1,3 +1,18 @@
+from django import forms
 from django.test import TestCase
 
-# Create your tests here.
+from nested_form_field import NestedFormField
+
+
+class TestSimpleNestedForm(TestCase):
+    def test(self):
+        class BookForm(forms.Form):
+            title = forms.CharField()
+
+        class SimpleForm(forms.Form):
+            book = NestedFormField(BookForm)
+
+        data = {"book_title": "Moby Dick"}
+        form = SimpleForm(data=data)
+        assert form.is_valid(), form.errors.as_text()
+        assert form.clean() == {"book": {"title": "Moby Dick"}}, form.cleaned_data
